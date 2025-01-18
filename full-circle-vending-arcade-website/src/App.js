fd3ce89d4f953fc159c3778a3b9bfd4b9aaa040f
fullcirclevendingarcade <fullcirclevendingarcade@gmail.com>
2025-01-18 11:27:21 -0700

    Initial Commit

---
+++ full-circle-vending-arcade-website/src/App.js
@@ -0,0 +1,27 @@
+import logo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Website Under Construction. We Apologize For Any Inconvenience. Stay Tuned!
+        </p>
+        {/*
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
